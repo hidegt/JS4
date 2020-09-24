f0dc50aa70e433238f55ffec8f82068f869cd591
Hideyo Gotoda <hidehide@HideyonoMacBook-Pro.local>
2020-09-24 18:46:52 +0900

    [finish] JS4

---
+++ app4.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("body").html("<h1>Hello jQuery!!</h1>");
+  // $( セレクタ ).メソッド ( パラメータ );
+});
+// $(document).ready(function () {
+  // jQueryプログラムの内容
+// });
+// or
+// $(function(){
+  // jQueryプログラムの内容
+// });
