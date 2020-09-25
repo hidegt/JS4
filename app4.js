b22827a9207db8e7e2f58625a44934ced4641282
Hideyo Gotoda <hidehide@HideyonoMacBook-Pro.local>
2020-09-25 17:34:44 +0900

    [finish]JS5

--- app4.js
+++ app4.js
@@ -1,7 +1,8 @@
-$(document).ready(function(){
-  $("body").html("<h1>Hello jQuery!!</h1>");
+// JS4
+// $(document).ready(function(){
+//   $("body").html("<h1>Hello jQuery!!</h1>");
   // $( セレクタ ).メソッド ( パラメータ );
-});
+// });
 // $(document).ready(function () {
   // jQueryプログラムの内容
 // });
@@ -9,3 +10,18 @@ $(document).ready(function(){
 // $(function(){
   // jQueryプログラムの内容
 // });
+
+// JS5
+$(function(){
+  $('.box1').hide();
+});
+
+$(function(){
+  $('.box2').slideDown(function(){
+    $('.box2').css({
+      'background-color': '#0000FF',
+      'height': '100px',
+      'width': '200px'
+    }).slideUp();
+  });
+});
