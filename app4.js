93509cdf2418f7cf4afa2c5deefd320bf41d307b
Hideyo Gotoda <hidehide@HideyonoMacBook-Pro.local>
2020-09-25 18:26:40 +0900

    [finish]JS6&7

--- app4.js
+++ app4.js
@@ -25,3 +25,36 @@ $(function(){
     }).slideUp();
   });
 });
+
+// JS6
+// $(function(){
+//   $('.box3').mouseover(function(){
+    // box1にマウスオーバーされたときに、処理を実行してください」の意味
+  //   $('.box3').addClass('box3-ext');
+  // });
+  // $('.box3').mouseout(function(){
+    // マウスアウトされたときに、背景色を赤色に変化させる処理
+//     $('.box3').removeClass('box3-ext');
+//   });
+// });
+$(function(){
+  $('.box3').on('click', function(){
+    $('.box3').addClass('box3-ext');
+  });
+  $('.box3').mouseout(function(){
+    $('.box3').removeClass('box3-ext');
+  });
+});
+
+// JS7
+$(function(){
+  $('.box4').on('click', function(){
+    $(this).slideUp();
+  });
+});
+
+$(function(){
+  $('button').on('click', function(){
+    $('ul').children().css('color', 'red');
+  });
+});
